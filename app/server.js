2b18062152949c77de325405f54395c782a8c2c4
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2018-02-26 11:51:33 -0800

    repair paths

--- app/server.js
+++ app/server.js
@@ -1,11 +1,13 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express()
 
 
-app.use(express.static('public')); // this path is relative to where node was started
+app.use(express.static(path.resolve(__dirname + '/public'))); // this path is relative to where node was started
 app.use(bodyParser.urlencoded({ extended: true })); // an epxress middleware that allows enables use of `req.body` to access parameters passed in url
 app.set('view engine', 'ejs')
+app.set('views',path.resolve(__dirname + '/views'))
 
 
 app.get('/', function (req, res) {
