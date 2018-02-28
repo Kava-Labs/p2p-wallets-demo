92f0014805309bd0276356ed10de361adec5a20d
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2018-02-28 10:32:09 -0800

    add new front end

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express()
 
 
-app.use(express.static(path.resolve(__dirname + '/public'))); // this path is relative to where node was started
+app.use(express.static(path.resolve(__dirname + '/public')));
 app.use(bodyParser.urlencoded({ extended: true })); // an epxress middleware that allows enables use of `req.body` to access parameters passed in url
 app.set('view engine', 'ejs')
 app.set('views',path.resolve(__dirname + '/views'))
@@ -14,6 +14,9 @@ app.get('/', function (req, res) {
   res.render('index', {result: null});
 })
 
+app.get('/login', function (req, res) {
+  res.render('login');
+})
 
 app.post('/', function (req, res) {
   console.log(req.body.from);
@@ -21,7 +24,7 @@ app.post('/', function (req, res) {
   console.log(req.body.amount);
   console.log('sending payment');
 	// TODO trigger an ILP payment
-  res.render('index', {result: 'payment sent!'});  
+	res.render('payment-processing');  
 })
 
 
