1cafa89626baa022ffa36890d27ecd06b49cf3e9
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2018-03-05 12:49:23 -0800

    add delay to log in

--- app/venmo-api.js
+++ app/venmo-api.js
@@ -29,7 +29,8 @@ class VenmoAPI {
 	
 	async login() {
 		await this.driver.get('https://venmo.com/account/sign-in');
-		await this.driver.findElement(By.name('phoneEmailUsername')).sendKeys(this.username);
+    await driver.wait(until.elementLocated(By.name('phoneEmailUsername')),5000).sendKeys(this.username);
+    debug(`Before sign in: ${this.driver.takeScreenshot())}`)
 		let passwordBox = await this.driver.findElement(By.name('password'));
 		await passwordBox.sendKeys(this.password)
 		await passwordBox.submit();
@@ -41,9 +42,10 @@ class VenmoAPI {
       // ignore timeout errors
       debug('timed out waiting for log in link')
     }
+    debug(`After sign in: ${this.driver.takeScreenshot())}`)
     let url = await this.driver.getCurrentUrl()
     debug(`Url of page after log in attempt: ${url}`)
-    if (url.toString() == 'https://venmo.com/account/mfa/verify-phone-email') {
+    if (url.toString() == 'https://venmo.com/account/mfa/code-prompt') {
       console.log('Venmo requires 2 factor authorization.')
       throw 'mfa error'
     } else {
@@ -57,9 +59,10 @@ class VenmoAPI {
     //await this.driver.get('https://venmo.com/account/mfa/code-prompt')
     try {
       debug('logging in in order to send mfa code')
-      await login()
+      await this.login()
     } catch (e) {
       // ignore mfa error
+      debug(`error in logging in: ${e}`)
     }
     debug('finding send mfa code button and clicking it')
     await this.driver.findElement(By.css('button.mfa-button-code-prompt')).click()
@@ -82,19 +85,20 @@ class VenmoAPI {
     let rememberButton = await this.driver.wait(
 			until.elementLocated(By.css('button.auth-button')),10000);
     
-    console.log('found button, clicking..')
+    debug('found button, clicking..')
     await rememberButton.click()
     //await login()
   }
 	
 	async connect(headless=true) {
-		console.log(`google_chrome_shim:${process.env.GOOGLE_CHROME_SHIM}, google_chrom_bin:${process.env.GOOGLE_CHROME_BIN}`)
+		debug('connecting venmo api')
+    debug(`google_chrome_shim:${process.env.GOOGLE_CHROME_SHIM}, google_chrom_bin:${process.env.GOOGLE_CHROME_BIN}`)
 		let options = new Chrome.Options()
 		if (headless) {
 			options = options.headless().windowSize({width: 640, height: 480})
 		}
 		if (CHROME_EXECUTABLE) {
-			console.log('Changing chrome path.')
+			debug('changing chrome path')
 			options = options.setChromeBinaryPath(CHROME_EXECUTABLE)
 		}
     //options = options.addArguments("--incognito");
