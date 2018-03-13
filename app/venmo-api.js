2a82e78dcf7dffa598c7b17489829080da12a6cc
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2018-03-12 21:56:26 -0700

    file tidy up

--- app/venmo-api.js
+++ app/venmo-api.js
@@ -23,15 +23,42 @@ const HEADLESS = JSON.parse(process.env.HEADLESS) // convert string to boolean
 
 
 class VenmoAPI {
-	constructor(username, password) {
-		this.username = username;
-		this.password = password;
-		this.driver = undefined;
-		this.lastLogInTime = 0;
-	}
-	
-	async login() {
-		await this.driver.get('https://venmo.com/account/sign-in');
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+    this.driver = undefined;
+    this.lastLogInTime = 0;
+  }
+
+  async connect() {
+    debug('connecting venmo api')
+    debug(`google_chrome_shim:${process.env.GOOGLE_CHROME_SHIM}, google_chrom_bin:${process.env.GOOGLE_CHROME_BIN}`)
+    let options = new Chrome.Options()
+    if (CHROME_EXECUTABLE) {
+      debug('changing chrome path')
+      options.setChromeBinaryPath(CHROME_EXECUTABLE)
+    }
+    const safeUserAgent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
+    options.addArguments(`user-agent="${safeUserAgent}"`)
+    // with headless chrome, UA is 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/64.0.3282.186 Safari/537.36'
+    if (HEADLESS) {
+      options.addArguments('headless')
+      options.windowSize({width: 1280, height: 800})
+    }
+    options.addArguments('disable-gpu','no-sandbox') // add arguments provided by the SHIM https://github.com/heroku/heroku-buildpack-google-chrome
+    options.addArguments(`proxy-server=${CHROME_PROXY}`) // routing chrome through a proxy https://www.systutorials.com/qa/247/how-to-set-google-chromes-proxy-settings-command-line-linux
+    debug('added arguments to chrome')
+    
+    this.driver = await new Builder()
+      .forBrowser('chrome')
+      .setChromeOptions(options)
+      .build();
+    
+    debug('built chrome driver')
+  }
+
+  async login() {
+    await this.driver.get('https://venmo.com/account/sign-in');
 
     await this.driver.sleep(3000); //TODO replace with `wait(until.titleIs` to detect when page has loaded
 
@@ -43,8 +70,8 @@ class VenmoAPI {
     
     await this.driver.wait(until.elementLocated(By.name('phoneEmailUsername')),5000).sendKeys(this.username);
     
-		let passwordBox = await this.driver.wait(until.elementLocated(By.name('password')),5000);
-		await passwordBox.sendKeys(this.password)
+    let passwordBox = await this.driver.wait(until.elementLocated(By.name('password')),5000);
+    await passwordBox.sendKeys(this.password)
     await this.driver.sleep(500)
     
     debug(`user agent: ${await this.driver.executeScript("return navigator.userAgent")}`)
@@ -54,7 +81,7 @@ class VenmoAPI {
         debug('Took before log in screenshot');
     });
     
-		await passwordBox.submit();
+    await passwordBox.submit();
     debug('submitted log in information')
     //wait to see which page it ends up at. Check url.
     try {
@@ -77,7 +104,7 @@ class VenmoAPI {
     } else {
       this.lastLogInTime = Date.now()
     }
-	}
+  }
   
   async send2FactorCode() {
     try {
@@ -97,7 +124,7 @@ class VenmoAPI {
   async submit2FactorAuth(authCode) {
     // assumes it's already at the right page left after running send2FactorCode
     let authCodeElement = await this.driver.wait(
-			until.elementLocated(By.name('token')),10000); //also class=auth-form-input
+      until.elementLocated(By.name('token')),10000); //also class=auth-form-input
     debug('submitting 2fa code')
     await authCodeElement.sendKeys(authCode);
     await authCodeElement.submit();
@@ -106,77 +133,50 @@ class VenmoAPI {
     await this.driver.sleep(5000);
     
     let rememberButton = await this.driver.wait(
-			until.elementLocated(By.css('button.auth-button')),10000);
+      until.elementLocated(By.css('button.auth-button')),10000);
     
     debug('found remember button, clicking')
     await rememberButton.click()
-    //await login()
   }
-	
-	async connect() {
-		debug('connecting venmo api')
-    debug(`google_chrome_shim:${process.env.GOOGLE_CHROME_SHIM}, google_chrom_bin:${process.env.GOOGLE_CHROME_BIN}`)
-		let options = new Chrome.Options()
-		if (CHROME_EXECUTABLE) {
-			debug('changing chrome path')
-			options.setChromeBinaryPath(CHROME_EXECUTABLE)
-		}
-    const safeUserAgent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'
-    options.addArguments(`user-agent="${safeUserAgent}"`)
-    // with headless chrome, UA is 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) HeadlessChrome/64.0.3282.186 Safari/537.36'
-    if (HEADLESS) {
-      options.addArguments('headless')
-      options.windowSize({width: 1280, height: 800})
+
+
+  async sendMoney(destinationUsername, amount, paymentMessage) {
+    await this.ensureLoggedIn()
+    // Load the peer's venmo page
+    await this.driver.get(`http://venmo.com/${destinationUsername}`);
+    // Select to pay them.
+    await this.driver.findElement(By.id('onebox_pay_toggle')).click() // using selenium promise manager thing
+    // Enter the payment amount.
+    await this.driver.findElement(By.id('onebox_details')).sendKeys(amount.toString() + ' ' + paymentMessage) // needs space to trigger the entry of 'for'
+    // Send the payment
+    await this.driver.findElement(By.id('onebox_send_button')).click()
+    // If the user has not been 
+  }
+
+  async getBalance() {
+    await this.ensureLoggedIn()
+    // Load main account page.
+    // await this.driver.get('http://venmo.com')
+    // Extract balance info
+    let balanceString = await this.driver.wait(
+      until.elementLocated(By.id('balance_right_side')), 20000
+    )
+    balanceString = await balanceString.getText()
+    // Drop initial $ sign. TODO check text matches pattern.
+    return parseFloat(balanceString.slice(1))
+  }
+
+  async ensureLoggedIn() {
+    // Log in if the last time we did so was more than 2 minutes ago.
+    // TODO Actually verify if browser is logged in.
+    if ((Date.now() - this.lastLogInTime) > 2*60*1000) {
+      await this.login()
     }
-    options.addArguments('disable-gpu','no-sandbox') // add arguments provided by the SHIM https://github.com/heroku/heroku-buildpack-google-chrome
-    options.addArguments(`proxy-server=${CHROME_PROXY}`) // routing chrome through a proxy https://www.systutorials.com/qa/247/how-to-set-google-chromes-proxy-settings-command-line-linux
-		debug('added arguments to chrome')
-    
-		this.driver = await new Builder()
-			.forBrowser('chrome')
-			.setChromeOptions(options)
-			.build();
-		
-    debug('built chrome driver')
-	}
-	
-	async sendMoney(destinationUsername, amount, paymentMessage) {
-		await this.ensureLoggedIn()
-		// Load the peer's venmo page
-		await this.driver.get(`http://venmo.com/${destinationUsername}`);
-		// Select to pay them.
-		await this.driver.findElement(By.id('onebox_pay_toggle')).click() // using selenium promise manager thing
-		// Enter the payment amount.
-		await this.driver.findElement(By.id('onebox_details')).sendKeys(amount.toString() + ' ' + paymentMessage) // needs space to trigger the entry of 'for'
-		// Send the payment
-		await this.driver.findElement(By.id('onebox_send_button')).click()
-		// If the user has not been 
-	}
-	
-	async getBalance() {
-		await this.ensureLoggedIn()
-		// Load main account page.
-		// await this.driver.get('http://venmo.com')
-		// Extract balance info
-		let balanceString = await this.driver.wait(
-			until.elementLocated(By.id('balance_right_side')), 20000
-			)
-		balanceString = await balanceString.getText()
-		// Drop initial $ sign. TODO check text matches pattern.
-		return parseFloat(balanceString.slice(1))
-	}
-	
-	async ensureLoggedIn() {
-		// Log in if the last time we did so was more than 2 minutes ago.
-		// TODO Actually verify if browser is logged in.
-		if ((Date.now() - this.lastLogInTime) > 2*60*1000) {
-			await this.login()
-		}
-	}
-	
-	disconnect() {
-		this.driver.quit()
-	}
+  }
+
+  disconnect() {
+    this.driver.quit()
+  }
 }
 
 module.exports = VenmoAPI
